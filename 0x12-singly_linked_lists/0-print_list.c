04c2cca05e414246e5ffe3895ee7c95455bbf9a2
Dennis-Ejike <onyishiejike@gmail.com>
2022-11-24 21:00:59 -0800

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,40 +1,23 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
-int main(void)
+
+size_t print_list(const list_t *h)
 {
-    list_t *h;
-    list_t *nnn;
-    list_t my_list = {"man", 5, NULL};
-    size_t nelem;
+	size_t nelem;
 
-    h = &my_list;
-    nnn = malloc(sizeof(list_t));
-    if (nnn == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("My_list");
-    nnn->len = 5;
-    nnn->next = h;
-    h = nnn;
-    nelem = print_list(h);
-    printf("-> %lu datas\n", nelem);
-
-    printf("\n");
-    free(nnn->str);
-    nnn->str = NULL;
-    n = print_list(h);
-    printf("-> %lu datas\n", nelem);
-
-    free(nnn);
-    return (0);
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
